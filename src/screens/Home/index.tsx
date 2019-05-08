7afa93be7a925050c4d74fe45738a851687916a5
Igor Ming de Mesquita <igormmesquita@ymail.com>
2019-05-08 00:41:47 -0300

    change home text

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,9 +5,7 @@ import Wrapper from './style';
 const Home = () => {
   return (
     <Wrapper.Container>
-      <Wrapper.Text.Title>
-        Welcome to React Native boilerplate!
-      </Wrapper.Text.Title>
+      <Wrapper.Text.Title>Welcome to TS boilerplate!</Wrapper.Text.Title>
       <Wrapper.Text.Description>
         This is to ensure the unit test is passing!
       </Wrapper.Text.Description>
