1f765478db16470a4d97d013c2e1054f8b5566c6
Igor Ming <igor.ming@gmail.com>
2023-10-27 23:38:23 -0300

    get rid of member references

--- template/android/app/src/main/java/com/member/MainActivity.java
+++ template/android/app/src/main/java/com/member/MainActivity.java
@@ -1,4 +1,4 @@
-package com.member;
+package com.tsboilerplate;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "member";
+    return "tsboilerplate";
   }
 
   /**
--- template/android/app/src/main/java/com/member/MainApplication.java
+++ template/android/app/src/main/java/com/member/MainApplication.java
@@ -1,4 +1,4 @@
-package com.member;
+package com.tsboilerplate;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
